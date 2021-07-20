517292b8cd44d2db520a1b9e8d0ad603662cfa8f
Phuc Leon <hachimanhikki191@gmail.com>
2021-07-20 07:01:21 +0700

    Frist commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Selecting all sections with class of section
+const sections = document.querySelectorAll('.section');
+console.log(sections);
+
+//On click event for each section
+sections.forEach((section) => {
+    section.addEventListener('click', () => {
+        //remove active class from all another section
+        //and add it to the selected section
+        sections.forEach((section) => {
+            section.classList.remove('active')
+        })
+        section.classList.add('active')
+    })
+})
